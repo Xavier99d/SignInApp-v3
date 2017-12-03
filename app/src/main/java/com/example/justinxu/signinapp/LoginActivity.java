8f814434bfa81c667e64a423f1247be25b61437a
Heman Kolla <hemankolla@hemans-air.attlocal.net>
2017-12-03 12:04:32 -0800

    Initial commit

---
+++ app/src/main/java/com/example/justinxu/signinapp/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.justinxu.signinapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        this.setTitle("Login Page");
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                System.out.println("Starting Intent");
+                Intent intent = new Intent(LoginActivity.this, UserAreaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
